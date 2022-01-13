7ce35b0ace30278bac9fded60c29f707515a72b1
roziqov1011 <roziqov1011@gmail.com>
2022-01-13 16:22:01 +0500

    date

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ TELEGRAM_BOT_TOKEN = '5099922790:AAHoZ_3dfP_plXlR_KnBb7xcGy2-fxMl2sU';
 const TeleBot = require('telebot');
 const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
 const chatIds = [];
+let date = new Date()
+let hours = date.getHours();
+let minutes = date.getMinutes();
 const CronJob = require('cron').CronJob;
 const job = new CronJob('0/5 * * * * *', function() {
   console.log('You will see this message every second');
   chatIds.forEach((chatId) => {
-      bot.sendMessage(chatId, 'Salom')
+      bot.sendMessage(chatId, `${hours}-${minutes}`)
   })
 }, null, true);
 
